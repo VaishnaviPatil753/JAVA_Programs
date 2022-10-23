ac379da795ea9da6aa5f9e5b7de8cd410830e939
VaishnaviPatil753 <114859651+VaishnaviPatil753@users.noreply.github.com>
2022-10-23 13:33:14 +0530

    Add files via upload

---
+++ 6.for_each_loop.java
@@ -0,0 +1,11 @@
+class for_each_loop
+{
+   public static void main(String arg[])
+  {
+     String s[]={"Prachi","sanika","Priya","Tanvi","Pooja"};
+     for(String b:s)
+    {
+     System.out.println(b);
+    }
+  }
+}
