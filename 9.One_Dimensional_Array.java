dd9cdcbc36be3b1af22d3313169d5c05651c975e
VaishnaviPatil753 <114859651+VaishnaviPatil753@users.noreply.github.com>
2022-10-27 23:14:02 +0530

    Add files via upload

---
+++ 9.One_Dimensional_Array.java
@@ -0,0 +1,11 @@
+class oned
+{
+   public static void main(String arg[])
+  {
+      int oned[]=new int[3];
+      oned[0]=10;
+      oned[1]=20;
+      oned[2]=oned[0]+oned[1];
+     System.out.println("Addition of array "+oned[2]);
+   }
+}
